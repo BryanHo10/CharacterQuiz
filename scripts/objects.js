3200e4e77866a45594313f0ea1afa18cb00f4bb5
BryanHo10 <bryankho1010@gmail.com>
2020-10-17 13:54:08 -0700

    Parse Entries into quiz Object

---
+++ scripts/objects.js
@@ -0,0 +1,22 @@
+let currentQuestionNum = 1;
+
+let globalCharactersList = [];
+
+/**
+ * Question:
+ *  - Text: String
+ *  - Answers: [Choice]
+ *  - Id: Number
+ */
+
+/**
+ * Choice:
+ *  - Text: String
+ *  - Character_Link: String
+ */
+
+let currentQuiz = {
+	title: null,
+	characters: [],
+	questions: [],
+};
--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,8 +1,5 @@
 const $ = window.jQuery;
 
-let currentQuestionNum = 1;
-let globalCharactersList = [];
-
 const addQuestionEntry = () => {
 	const $element = $('[id="questions-container"]');
 	currentQuestionNum += 1;
@@ -10,7 +7,7 @@ const addQuestionEntry = () => {
             <div class="py-4">
                 <h4>Question ${currentQuestionNum}</h4>
                 <hr />
-                <input class="form-control form-control-lg" type="text" placeholder="Enter Question Text">
+                <input id="question-title-${currentQuestionNum}" class="form-control form-control-lg" type="text" placeholder="Enter Question Text">
                 <!-- Current Answer Start -->
                 <div id="choice-container-${currentQuestionNum}">
                     <div class="row py-3">
@@ -20,6 +17,10 @@ const addQuestionEntry = () => {
                         <div class="col-md-6">
                             <select id="inputCharacter" class="form-control">
                             <option selected value="" disabled>Choose...</option>
+                            ${globalCharactersList.map(
+															(label) =>
+																`<option value="${label}" >${label}</option>`
+														)}
                             </select>
                         </div>
                     </div>
@@ -73,14 +74,57 @@ const addChoiceEntry = (questionNum) => {
         <div class="col-md-6">
             <select id="inputCharacter" class="form-control">
                 <option selected value="" disabled>Choose...</option>
+                ${globalCharactersList.map(
+									(label) => `<option value="${label}" >${label}</option>`
+								)}
             </select>
         </div>
         </div><hr/>`);
-	updateCharacterList();
 	$element.append($newChoice);
 };
 const submitQuiz = () => {
 	event.preventDefault();
+	saveQuestions();
 	console.log("All Done");
 	// window.location.href = `http://127.0.0.1:3000/index.html`;
+};
+const saveQuestions = () => {
+	const characterList = [...globalCharactersList];
+	const titleEntry = $(`[id="quiz-title"]`).val();
+	const choiceEntries = Array.from(Array(currentQuestionNum)).map(
+		(_, index) => {
+			let allChoiceTexts = [];
+			$(`[id="choice-container-${index + 1}"]`)
+				.find("input")
+				.each(function () {
+					allChoiceTexts.push($(this).val());
+				});
+
+			let allCharacterChoices = [];
+			$(`[id="choice-container-${index + 1}"]`)
+				.find("select")
+				.each(function () {
+					allCharacterChoices.push($(this).children("option:selected").val());
+				});
+			return allChoiceTexts.map((val, index) => {
+				return {
+					text: val,
+					character: allCharacterChoices[index],
+				};
+			});
+		}
+	);
+	const questionEntries = Array.from(Array(currentQuestionNum)).map(
+		(_, index) => {
+			return {
+				text: $(`[id="question-title-${index + 1}"]`).val(),
+				id: index + 1,
+				answers: choiceEntries[index],
+			};
+		}
+	);
+	currentQuiz.characters = characterList;
+	currentQuiz.title = titleEntry;
+	currentQuiz.questions = questionEntries;
+	console.log(currentQuiz);
 };
