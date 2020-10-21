246af6d5b671a69c4397e7dd1f45dabd4b9e7031
BryanHo10 <bryankho1010@gmail.com>
2020-10-20 21:50:43 -0700

    Added page to view and take quizzes

--- scripts/dataStore.js
+++ scripts/dataStore.js
@@ -1,3 +1,37 @@
-const saveQuiz = () => {};
+const remoteURL = "http://107.185.99.52/CharacterQuiz-API/";
+
+const saveQuiz = (id, value) => {
+	localStorage.setItem();
+};
 const getAllQuizzes = () => {};
 const removeQuiz = () => {};
+
+// Mock Data
+const mockGetAllQuizzes = () => {
+	return Promise((resolve) => {
+		resolve([
+			{
+				title: "Title",
+				characters: ["Character1", "Character2", "Character3"],
+				questions: [
+					{
+						text: "Question1",
+						id: 1,
+						answers: [
+							{ text: "Answer1", character: "Character1" },
+							{ text: "Answer2", character: "Character2" },
+						],
+					},
+					{
+						text: "Question2",
+						id: 2,
+						answers: [
+							{ text: "Answer1", character: "Character1" },
+							{ text: "Answer2", character: "Character2" },
+						],
+					},
+				],
+			},
+		]);
+	});
+};
---
+++ scripts/home.js
@@ -0,0 +1,43 @@
+const $ = window.jQuery;
+const populateDashboard = () => {
+	//Add fetch to populate questionLength
+	const questionLength = 4;
+	const QuestionPreviewContainer = $(`[id="quiz-preview-container"]`);
+	if (questionLength === 0) {
+		const NoQuestions = $(`<div class="text-muted text-center">
+            <h1>No Questions</h1>
+            <hr />
+        </div>`);
+		QuestionPreviewContainer.append(NoQuestions);
+		return;
+	}
+	// QuestionPreviewContainer.empty();
+	for (let i in [...Array(questionLength)]) {
+		const quizPreviewTemplate = $(`<div class="card mb-4 shadow-sm">
+            <div class="card-body">
+            <h2 class="card-title">Sample Title</h2>
+            <hr />
+            <div class="py-4"></div>
+            <div class=" d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                <button type="button" class="btn btn-lg btn-outline-secondary" onclick="viewQuiz(${i})">View</button>
+                </div>
+                <div class="row w-50 justify-content-end">
+                <div class="px-5 text-center">
+                    <h3>6</h3>
+                    Characters
+                </div>
+                <div class="px-5 text-center">
+                    <h3>6</h3>
+                    Questions
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>`);
+		QuestionPreviewContainer.append(quizPreviewTemplate);
+	}
+};
+const viewQuiz = (id) => {
+	window.location.href = `/view-quiz.html`;
+};
